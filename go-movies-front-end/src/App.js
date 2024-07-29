ab055f73d9171b33e944f559a7d27d1aff5b2b98
Will <gostjoketw@gmail.com>
2024-07-29 00:20:33 -0700

    07292024

--- go-movies-front-end/src/App.js
+++ go-movies-front-end/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import Alert from "./components/Alert";
 
@@ -13,6 +13,25 @@ function App() {
     setJwtToken("");
     navigate("/login");
   }
+
+  useEffect(()=>{
+    if (jwtToken === ""){
+      const requestOptions = { 
+        methods:"GET", 
+        credentials: "include",
+      }
+      fetch(`/fresh`, requestOptions)
+      .then((response) => response.json())
+      .then((data)=>{
+        if (data.access_token){
+          setJwtToken(data.access_token);
+        }
+      })
+      .catch(error =>{
+        console.log("user is not logged in", error)
+      })
+    }
+  }, [jwtToken]);
 
   return (
     <div className="container">
